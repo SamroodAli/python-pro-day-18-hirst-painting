29bf39cdec57b996585e6f115d38b04b13c01ed5
SamroodAli <samroodali@live.com>
2020-12-21 17:36:50 +0530

    Add: 10*10 random dots

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import colorgram
+from turtle import Turtle, Screen
+from random import choice
+
 from collections import namedtuple
 colors = colorgram.extract("images/image.jpg", 35)
 colors_tuples = []
@@ -8,3 +11,22 @@ for color in colors:
     b = color.rgb.b
     colors_tuples.append((r, g, b))
 
+print(colors_tuples)
+# code for turtle
+t = Turtle()
+scr = Screen()
+scr.colormode(255)
+t.penup()
+t.speed(0)
+sety = -200
+t.setposition(-200, -200)
+for position in range(1, 101):
+    color = choice(colors_tuples)
+    t.dot(20, color)
+    t.forward(50)
+    if position % 10 == 0:
+        sety += 50
+        t.setposition(-200, sety)
+
+
+scr.exitonclick()
